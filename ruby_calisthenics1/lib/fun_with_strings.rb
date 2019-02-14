693a5ec57da28fb99816c5147b6f28e622c70eea
Jack Hitchcock <Jhitchc5@uncc.edu>
2019-02-14 00:48:10 +0000

    Lab 9: Fun with words - Palindrome

--- ruby_calisthenics1/lib/fun_with_strings.rb
+++ ruby_calisthenics1/lib/fun_with_strings.rb
@@ -1,12 +1,16 @@
 module FunWithStrings
   def palindrome?
-    # your code here
+    p = self.downcase.gsub /[^a-z0-9]+/i, ''
+    p == p.reverse
   end
   def count_words
-    # your code here
+       w = Hash.new(0)
+    p = self.downcase.gsub /[^a-z0-9 ]+/i, ''
+    p.split.each { |x| w[x] = w[x] + 1 }
+    w
   end
   def anagram_groups
-    # your code here
+    p = self.downcase.split.group_by{ |x| x.chars.sort }.values
   end
 end
 
